f61f7fd1ff0f2a96ff6c428e8ef2a057462db178
Max Gurewitz <max@dittofeed.com>
2023-06-29 16:58:33 -0700

    init method

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { DittofeedReactNative } from "@dittofeed/sdk-react-native";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {});
   new DittofeedReactNative();
   return (
     <View style={styles.container}>
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,39 @@
+import notifee, { EventType, Event } from "@notifee/react-native";
+import { useEffect } from "react";
+import messaging from "@react-native-firebase/messaging";
+
 export class DittofeedReactNative {
-    constructor() {
-        console.log('DittofeedReactNative constructor');
+  private static instance: DittofeedReactNative;
+
+  constructor() {
+    console.log("DittofeedReactNative constructor");
+  }
+
+  static async getInstance(): Promise<DittofeedReactNative> {
+    if (!DittofeedReactNative.instance) {
+      DittofeedReactNative.instance = new DittofeedReactNative();
+      await DittofeedReactNative.instance.init();
     }
+    return DittofeedReactNative.instance;
+  }
+
+  private async init() {
+    await messaging().registerDeviceForRemoteMessages();
+    const token = await messaging().getToken();
+    // TODO inject overridable logger
+    void this.submitToken(token).catch(console.error);
+    // TODO allow channel to be overridden
+    await notifee.createChannel({
+      id: "default",
+      name: "Default Channel",
+      visibility: 1,
+      importance: 4,
+    });
+    messaging().onTokenRefresh(this.submitToken);
+    notifee.onBackgroundEvent(this.handlBackgroundEvent);
+  }
+
+  private async submitToken(token: string): Promise<void> {}
+
+  private async handlBackgroundEvent(event: Event): Promise<void> {}
 }
