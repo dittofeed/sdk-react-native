ea793f7c0edb9bbcd1cae1e07846ba37efdfedc4
Max Gurewitz <max@dittofeed.com>
2023-06-29 17:49:25 -0700

    restructure around peer dep

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import notifee, { EventType, Event } from "@notifee/react-native";
-import { useEffect } from "react";
-import messaging from "@react-native-firebase/messaging";
+import notifee, { Event } from "@notifee/react-native";
+import type { FirebaseMessagingTypes } from "@react-native-firebase/messaging";
+
+interface DittofeedReactNativeInit {
+  messaging: FirebaseMessagingTypes.Module;
+}
 
 export class DittofeedReactNative {
   private static instance: DittofeedReactNative;
@@ -9,17 +12,19 @@ export class DittofeedReactNative {
     console.log("DittofeedReactNative constructor");
   }
 
-  static async getInstance(): Promise<DittofeedReactNative> {
+  static async getInstance(
+    initParams: DittofeedReactNativeInit
+  ): Promise<DittofeedReactNative> {
     if (!DittofeedReactNative.instance) {
       DittofeedReactNative.instance = new DittofeedReactNative();
-      await DittofeedReactNative.instance.init();
+      await DittofeedReactNative.instance.init(initParams);
     }
     return DittofeedReactNative.instance;
   }
 
-  private async init() {
-    await messaging().registerDeviceForRemoteMessages();
-    const token = await messaging().getToken();
+  private async init({ messaging }: DittofeedReactNativeInit) {
+    await messaging.registerDeviceForRemoteMessages();
+    const token = await messaging.getToken();
     // TODO inject overridable logger
     void this.submitToken(token).catch(console.error);
     // TODO allow channel to be overridden
@@ -29,11 +34,15 @@ export class DittofeedReactNative {
       visibility: 1,
       importance: 4,
     });
-    messaging().onTokenRefresh(this.submitToken);
+    messaging.onTokenRefresh(this.submitToken);
     notifee.onBackgroundEvent(this.handlBackgroundEvent);
   }
 
-  private async submitToken(token: string): Promise<void> {}
+  private async submitToken(token: string): Promise<void> {
+    console.log("submitToken", token);
+  }
 
-  private async handlBackgroundEvent(event: Event): Promise<void> {}
+  private async handlBackgroundEvent(event: Event): Promise<void> {
+    console.log("handlBackgroundEvent", event);
+  }
 }
