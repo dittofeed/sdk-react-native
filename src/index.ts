31ad0c811285ea6f450203884d78bf29d94a7f02
Max Gurewitz <max@dittofeed.com>
2023-07-05 11:16:58 -0700

    pseudocode

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,63 @@
-import notifee, { Event } from "@notifee/react-native";
-import messaging from "@react-native-firebase/messaging";
+import notifee, { Event, EventType } from "@notifee/react-native";
+import { ModuleWithStatics } from "@notifee/react-native/dist/types/Module";
+import { ReactNativeFirebase } from "@react-native-firebase/app";
+import messaging, {
+  FirebaseMessagingTypes,
+} from "@react-native-firebase/messaging";
 
 interface DittofeedReactNativeInit {}
+
+type Firebase = ReactNativeFirebase.FirebaseModuleWithStatics<
+  FirebaseMessagingTypes.Module,
+  FirebaseMessagingTypes.Statics
+>;
+
+type DisplayNotificationParam = Parameters<
+  typeof notifee.displayNotification
+>[0];
+
+type OnBackgroundEvent = (
+  preRendered: DisplayNotificationParam,
+  event: Event
+) => Promise<DisplayNotificationParam>;
+
+export class DittofeedReactNative2 {
+  notifee: ModuleWithStatics;
+  firebase: Firebase;
+  onBackgroundEvent: OnBackgroundEvent;
+
+  constructor(
+    notifee: ModuleWithStatics,
+    firebase: Firebase,
+    onBackgroundEvent: OnBackgroundEvent
+  ) {
+    this.notifee = notifee;
+    this.firebase = firebase;
+    this.onBackgroundEvent = onBackgroundEvent;
+  }
+
+  async init() {
+    notifee.onBackgroundEvent(async (event) => {
+      const { type } = event;
+
+      if (type !== EventType.DELIVERED) {
+        void this.trackEvent(event);
+      }
+      const rendered = this.renderEvent(event);
+      if (!rendered) {
+        return;
+      }
+      const display = await this.onBackgroundEvent(rendered, event);
+      await notifee.displayNotification(display);
+    });
+  }
+
+  renderEvent(event: Event): DisplayNotificationParam | null {
+    return null;
+  }
+
+  async trackEvent(event: Event): Promise<void> {}
+}
 
 // FIXME rename to DittofeedSDK
 export class DittofeedReactNative {
