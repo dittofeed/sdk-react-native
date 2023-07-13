43d7beb5db31bb13df0e4d2f21415cecb5dd2d4a
Max Gurewitz <max@dittofeed.com>
2023-07-13 14:23:47 -0700

    updated example

--- example/App.tsx
+++ example/App.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { DittofeedReactNative } from "@dittofeed/sdk-react-native";
+import { DittofeedSdk } from "./sdk";
 import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    DittofeedReactNative.getInstance();
+    (async () => {
+      await DittofeedSdk.init({
+        writeKey:
+          "Basic ZGNlODRiYzctMzBiMi00Y2VjLTgxMGMtNTc3ZjFhYmViODZhOmYxODFhYWRjMWU1ZTllNDY=",
+        host: "http://10.0.2.2:3001",
+      });
+      await DittofeedSdk.identify({
+        userId: "123",
+        traits: {},
+      });
+    })();
   }, []);
 
   return (
